5298320b766e6b6a85f9e719074b0eef4b5f5aa2
ikenwaa <ajohn9353@gmail.com>
2019-11-26 17:03:01 +0100

    added little tweaks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import HomePage from './pages/homepage/homepage.comp';
-import './App.css';
+import React from "react";
+// import { Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.comp";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+	return (
+		<div>
+			<HomePage />
+		</div>
+	);
 }
 
 export default App;
