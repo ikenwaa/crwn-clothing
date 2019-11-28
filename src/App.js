e0075ebcbbac4d580460579569a69cfa06a811a1
ikenwaa <ajohn9353@gmail.com>
2019-11-28 14:24:23 +0100

    added new components and style rules

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.comp";
 import ShopPage from './pages/shop/shop.comp';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.comp';
 import Header from './components/header/header.comp'
 import "./App.css";
 
@@ -11,7 +12,7 @@ function App() {
 			<Header />
 			<Switch>
 				<Route exact path='/' component={HomePage} />
-				<Route exact path='/shop' component={ShopPage} />
+				<Route exact path='/sign-in' component={SignInAndSignUpPage} />
 				<Route />
 			</Switch>
 		</div>
